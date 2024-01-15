42cbf760b2c9ce1f6d9e181ef671b6f26aca7938
jun-kondo <ranmaru.oo.q@gmail.com>
2024-01-15 15:59:03 +0900

    [Add]カレンダープログラムの作成
    カレンダープログラムを作成しました。

---
+++ 02.calendar/calendar.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+require 'date'
+require 'optparse'
+
+# コマンドラインオプション設定(デフォルト値は今日の年と月)
+params =ARGV.getopts("m:", "y:", "m:#{Date.today.mon}", "y:#{Date.today.year}")
+input_month = params["m"].to_i
+input_year = params["y"].to_i
+
+# 月の最初の日にちを取得
+start_date = Date.new(input_year, input_month, 1)
+# 月の最終日を取得
+end_date = Date.new(start_date.year, start_date.mon, -1)
+
+# 月と年を取得
+header = "#{start_date.mon}月 #{start_date.year}"
+
+# すべての曜日を取得
+week_days = ["日", "月", "火", "水", "木", "金", "土"]
+
+# カレンダーの最初の日までの余白を配列で作成
+margin_space = []
+start_date.wday.times do
+  margin_space.unshift("  ")
+end
+
+# すべての日付を配列で作成
+all_days = (start_date.day..end_date.day).to_a
+
+# 配列の要素を文字列型に変更、1日ではない&&土曜日で改行, 各要素を右寄せ
+calendar_formatted = all_days.map do |day|
+  date = Date.new(start_date.year, start_date.mon, day)
+  day != 1 && date.sunday? ? "\n" + day.to_s.rjust(2) : day.to_s.rjust(2)
+end
+# 余白と日付の配列を連結
+calendar = margin_space + calendar_formatted
+
+# カレンダーを表示
+puts header.center(20)
+puts week_days.join(" ")
+puts calendar.join(" ")
+
