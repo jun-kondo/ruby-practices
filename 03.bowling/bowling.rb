3a261c606029dbe0ba7a66adc7c8471a3e3626a4
jun-kondo <ranmaru.oo.q@gmail.com>
2024-01-23 12:50:38 +0900

    [Modify]メソッド部分をbowling_methods.rbに切り出し
    メソッドのテストがやりやすいようにメソッド部分を別ファイルに切り出しました。

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require_relative 'bowling_methods'
+
 score = ARGV[0]
 
 scores = score.split(',')
@@ -19,36 +21,4 @@ shots.each_slice(2) do |s|
   frames << s
 end
 
-def strike_bonus(frames, index)
-  next_frame = frames[index + 1]
-  if next_frame[0] == 10
-    after_next_frame = frames[index + 2]
-    next_frame[0] + after_next_frame[0]
-  else
-    next_frame.sum
-  end
-end
-
-def spare_bonus(frames, index)
-  next_frame = frames[index + 1]
-  next_frame[0]
-end
-
-def culc_score(frames)
-  point = 0
-  frames.each_with_index do |frame, index|
-    if frame[0] == 10
-      point += 10
-      point += strike_bonus(frames, index)
-    elsif frame.sum == 10
-      point += 10
-      point += spare_bonus(frames, index)
-    else
-      point += frame.sum
-    end
-    break if index == 9
-  end
-  puts point
-end
-
-culc_score(frames)
+puts culc_score(frames)
