bd9df08439e4e88060f606c4afe7b7724d62174b
jun-kondo <ranmaru.oo.q@gmail.com>
2024-01-25 15:47:49 +0900

    [Add]オプション無しのlsコマンドを作成

---
+++ 04.ls/ls.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+def main
+  file_names = gets_file_names
+
+  arranged_file_names = arrange_names(file_names)
+
+  file_names_lists = creat_lists(arranged_file_names)
+
+  output(file_names_lists)
+end
+
+def gets_file_names
+  Dir.glob('*').map { |file| File.basename(file) }
+end
+
+def width(str)
+  str.size + str.chars.count { |char| !char.ascii_only? }
+end
+
+def arrange_names(names)
+  max_word = names.max { |a, b| width(a) <=> width(b) }
+  names.sort.map do |name|
+    if name.ascii_only?
+      name.ljust(width(max_word) + 1)
+    else
+      name.ljust(max_word.size + 1)
+    end
+  end
+end
+
+def creat_lists(arranged_names, col: 3)
+  row = arranged_names.size <= col ? 1 : arranged_names.size / col + 1
+  arranged_names.each_slice(row).map { |divided_names| divided_names + Array.new((row - divided_names.size), '') }.transpose
+end
+
+def output(names_lists)
+  names_lists.each { |names_list| names_list.each { |name| name == names_list.last ? puts(name) : print(name) } }
+end
+
+main
