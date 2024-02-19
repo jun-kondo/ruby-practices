3126bd1b49eec4d8c1eb20f21caa35872d6dc084
jun-kondo <ranmaru.oo.q@gmail.com>
2024-02-19 14:38:29 +0900

    [Add]オプションで表示項目を限定する機能を追加
    コマンドラインでオプションを受取りその内容を表示する機能を追加

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -13,45 +13,57 @@ class WcCommand
   end
 
   def main
-    # options = argv[:options]
+    options = argv[:options]
     file_names = argv[:file_names]
-    counter_list = counter(file_names)
+    counter_list = counter(file_names, options)
     calc_total(counter_list).map do |list|
-      [
-        list[:line].to_s.rjust(8),
-        list[:word].to_s.rjust(7),
-        list[:byte].to_s.rjust(7),
-        list[:file_name]
-      ].join(' ')
+      low = ['']
+      list[:count].each_value { |v| low << v.to_s.rjust(7) }
+      low << list[:file_name]
+      low.join(' ')
     end.join("\n")
   end
 
   def calc_total(counter_list)
     if counter_list.size > 1
-      total_amount = {
-        line: counter_list.map { _1[:line] }.inject(:+),
-        word: counter_list.map { _1[:word] }.inject(:+),
-        byte: counter_list.map { _1[:byte] }.inject(:+),
-        file_name: 'total'
-      }
+      total_amount = { file_name: 'total', count: {} }
+      counter_list.map do |low|
+        low[:count].each_key do |k|
+          total_amount[:count][k] = counter_list.map { _1[:count][k] }.inject(:+)
+        end
+      end
       counter_list << total_amount
     else
       counter_list
     end
   end
 
-  def counter(file_names)
+  def counter(file_names, options)
     file_names.map do |file_name|
       File.open(file_name) do |f|
         words = f.readlines
-        {
-          line: words.map { |w| w.count("\n") }.inject(:+),
-          word: words.join.split.size,
-          byte: words.join.bytesize,
-          file_name:
-        }
+        if options.value?(true)
+          optional_count(words, options, file_name)
+        else
+          {
+            file_name:,
+            count: {
+              line: words.map { |w| w.count("\n") }.inject(:+),
+              word: words.join.split.size,
+              byte: words.join.bytesize
+            }
+          }
+        end
       end
     end
+  end
+
+  def optional_count(words, options, file_name)
+    low = { file_name:, count: {} }
+    low[:count][:line] = words.map { |w| w.count("\n") }.inject(:+) if options['l']
+    low[:count][:word] = words.join.split.size if options['w']
+    low[:count][:byte] = words.join.bytesize if options['c']
+    low
   end
 end
 
