733472f2f35eb3f0d1d75a5f3e69c66b4209f93b
jun-kondo <ranmaru.oo.q@gmail.com>
2024-04-17 19:42:53 +0900

    コメント削除

--- 06.bowling_object/lib/bowling_result_parser.rb
+++ 06.bowling_object/lib/bowling_result_parser.rb
@@ -21,9 +21,6 @@ class BowlingResultParser
   private
 
   def separate(all_pins)
-    # separated_pins = []
-    # all_pins.each_slice(2) { |pins| separated_pins << pins }
     all_pins.each_slice(2).to_a
-    # separated_pins
   end
 end
