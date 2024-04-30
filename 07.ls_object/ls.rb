d3551ae3988569f1e76490c49b5fa88f490d5fa8
jun-kondo <ranmaru.oo.q@gmail.com>
2024-04-30 19:18:44 +0900

    [Modify]実行ファイル(ls.rb)
    long_formatも動作するように。

--- 07.ls_object/ls.rb
+++ 07.ls_object/ls.rb
@@ -2,7 +2,10 @@
 # frozen_string_literal: true
 
 require_relative 'ls_command'
+require_relative 'file'
 require_relative 'short_format'
+require_relative 'long_format'
 
 ls = LsCommand.new(ARGV)
-puts ls.format_mode.new(ls.filenames_order).output
+files = ls.filenames_order.map { |filename| Ls::File.new(filename) }
+puts ls.format_mode.new(files).output
