08728a4d24a4593240425da0342ba8db8cfa0f3d
jun-kondo <ranmaru.oo.q@gmail.com>
2024-04-30 19:21:03 +0900

    [Modify]short_formatクラス
    fileオブジェクトを使用する(name属性のみ)よう変更

--- 07.ls_object/short_format.rb
+++ 07.ls_object/short_format.rb
@@ -5,50 +5,56 @@ class ShortFormat
   # SPACE_CHARACTER = ' '
   # SPACE_CHAR_COUNT_BETWEEN_FILENAMES = 1
 
-  def initialize(filenames)
-    @filenames = filenames
-    @file_count = filenames.size
+  def initialize(files)
+    @files = files
+    @file_count = files.size
   end
 
   def output
     # create_filenames_matrix(arrange_filenames).map(&:join).join("\n")
-    create_filenames_matrix(arrange_filenames).map { |row| row.join(' ') }.join("\n")
+    generate_filenames_matrix.map { |row| row.join(' ') }.join("\n")
   end
 
   private
 
-  def create_filenames_matrix(arranged_names)
-    arranged_names.each_slice(row_count).map { |divided_names| divided_names + Array.new((row_count - divided_names.size), '') }.transpose
+  def generate_filenames_matrix
+    arrange_filenames.each_slice(row_count).map { |divided_names| generate_filenames_row(divided_names) }.transpose
+  end
+
+  def arrange_filenames
+    @files.map { |file| multibyte_ljust(file) }
   end
 
   def row_count
     @row_count ||= (@file_count % COL_COUNT).zero? ? @file_count / COL_COUNT : @file_count.fdiv(COL_COUNT).ceil
   end
 
-  def arrange_filenames
-    @filenames.map { |filename| multibyte_ljust(filename) }
+  def generate_filenames_row(filenames)
+    filenames + Array.new((row_count - filenames.size), '')
   end
 
   # filenameクラスに出来そう
-  def multibyte_ljust(filename, padding: ' ')
-    filename + padding * padding_size(filename)
+  def multibyte_ljust(file, padding: ' ')
+    file.name + padding * padding_size(file)
   end
 
   # filenameクラスに出来そう
-  def padding_size(filename)
-    max_length_filename_width - display_file_width(filename)
+  # レシーバはファイルクラス
+  def padding_size(file)
+    max_length_filename_width - file.display_name_width
   end
 
   def max_length_filename_width
-    @max_length_filename_width ||= display_file_width(max_length_filename)
+    @max_length_filename_width ||= max_name_length_file.display_name_width
   end
 
-  def max_length_filename
-    @filenames.max_by { |filename| display_file_width(filename) }
+  # ファイルオブジェクトを返す
+  def max_name_length_file
+    @files.max_by(&:display_name_width)
   end
 
   # filenameクラスに出来そう
-  def display_file_width(filename)
-    filename.size + filename.chars.count { |char| !char.ascii_only? }
-  end
+  # def display_file_width(filename)
+  #   filename.size + filename.chars.count { |char| !char.ascii_only? }
+  # end
 end
